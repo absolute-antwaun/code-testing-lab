8baa5a300fdf22b1f6822a035584073b68505bd5
Antwaun Westerfield <157628094+absolute-antwaun@users.noreply.github.com>
2025-02-04 07:52:05 -0800

    Create cake_sale.py

---
+++ cake_sale.py
@@ -0,0 +1,32 @@
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("~~~~~~               ~~~~~~")
+print("~~~~~~   Cake Sale   ~~~~~~")
+print("~~~~~~               ~~~~~~")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("")
+
+print("Welcome To Our Cake Sale!")
+
+print("Here are the cake for sale:")
+
+print(" > Cupcakes")
+print(" > Macarons")
+print(" > Cheesecake")
+print("")
+
+cupcake_price = 0.40
+macarons_price = 0.50
+cheesecake_price = 0.70
+
+cupcake = int(input("How many cupcakes do you plan to sell? "))
+cupcake_total = cupcake * cupcake_price
+
+macarons = int(input("How many macarons do you plan to sell? "))
+macarons_total = macarons * macarons_price
+
+cheesecake = int(input("How many cheesecakes do you plan to sell? "))
+cheesecake_total = cheesecake * cheesecake_price
+
+cake_sale = cupcake_total + macarons_total + cheesecake_total
+
+print(f"You will make ${cake_sale} profit")
